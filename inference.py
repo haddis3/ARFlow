3f3219ecd4b5cb63286abdc45948ab1e712aeb19
haddis3 <nick2nie@gmail.com>
2021-02-05 13:54:30 +0800

    Add files via upload
    rectify inference for batch examples and generate image matrix

--- inference.py
+++ inference.py
@@ -2,6 +2,7 @@ import imageio
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 import torch
 from easydict import EasyDict
@@ -9,8 +10,20 @@ from torchvision import transforms
 from transforms import sep_transforms
 
 from utils.flow_utils import flow_to_image, resize_flow
+from utils.warp_utils import flow_warp, get_occu_mask_backward
 from utils.torch_utils import restore_model
 from models.pwclite import PWCLite
+from skimage import img_as_ubyte
+
+
+def get_occu_mask_bidirection(flow12, flow21, scale=0.1, bias=0.5):
+    flow21_warped = flow_warp(flow21, flow12, pad='zeros')
+    flow12_diff = flow12 + flow21_warped
+    mag = (flow12 * flow12).sum(1, keepdim=True) + \
+          (flow21_warped * flow21_warped).sum(1, keepdim=True)
+    occ_thresh = scale * mag + bias
+    occ = (flow12_diff * flow12_diff).sum(1, keepdim=True) > occ_thresh
+    return occ.float()
 
 
 class TestHelper():
@@ -41,10 +54,14 @@ class TestHelper():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', '--model', default='checkpoints/KITTI15/pwclite_ar.tar')
-    parser.add_argument('-s', '--test_shape', default=[384, 640], type=int, nargs=2)
+    parser.add_argument('-m', '--model', default='checkpoints/Sintel/pwclite_ar.tar')
+    # parser.add_argument('-m', '--model', default='checkpoints/Sintel/111320_ckpt.pth.tar')
+    parser.add_argument('-s', '--test_shape', default=[512, 512], type=int, nargs=2)
     parser.add_argument('-i', '--img_list', nargs='+',
                         default=['examples/img1.png', 'examples/img2.png'])
+    parser.add_argument('-list', '--test_list', nargs='+',
+                        default='data/flow_dataset/ceshi/left')
+
     args = parser.parse_args()
 
     cfg = {
@@ -59,16 +76,61 @@ if __name__ == '__main__':
 
     ts = TestHelper(cfg)
 
-    imgs = [imageio.imread(img).astype(np.float32) for img in args.img_list]
-    h, w = imgs[0].shape[:2]
+    for name in os.listdir(args.test_list):
 
-    flow_12 = ts.run(imgs)['flows_fw'][0]
+        print('processing {}'.format(name))
+        path = []
+        right = os.path.join('data/flow_dataset/ceshi/right', name)
+        path.append(os.path.join(args.test_list, name))
+        path.append(right)
+        imgs = [imageio.imread(img).astype(np.float32) for img in path]
+        h, w = imgs[0].shape[:2]
 
-    flow_12 = resize_flow(flow_12, (h, w))
-    np_flow_12 = flow_12[0].detach().cpu().numpy().transpose([1, 2, 0])
+        res_dict = ts.run(imgs)
+        flow_12 = res_dict['flows_fw'][0]
+        flow_21 = res_dict['flows_bw'][0]
 
-    vis_flow = flow_to_image(np_flow_12)
+        flow_12 = resize_flow(flow_12, (h, w))  # [1, 2, H, W]
+        flow_21 = resize_flow(flow_21, (h, w))  # [1, 2, H, W]
+        occu_mask1 = 1 - get_occu_mask_bidirection(flow_12, flow_21)  # [1, 1, H, W]
+        occu_mask2 = 1 - get_occu_mask_bidirection(flow_21, flow_12)
+        back_occu_mask1 = get_occu_mask_backward(flow_21)
+        back_occu_mask2 = get_occu_mask_backward(flow_21)
 
-    fig = plt.figure()
-    plt.imshow(vis_flow)
-    plt.show()
+        warped_image_12 = flow_warp(torch.from_numpy(np.transpose(imgs[1], [2, 0, 1])).unsqueeze(0).cuda(), flow_12, pad='border')
+        warped_image_21 = flow_warp(torch.from_numpy(np.transpose(imgs[0], [2, 0, 1])).unsqueeze(0).cuda(), flow_21, pad='border')
+        np_warped_image12 = warped_image_12[0].detach().cpu().numpy().transpose([1, 2, 0])
+        np_warped_image21 = warped_image_21[0].detach().cpu().numpy().transpose([1, 2, 0])
+
+        np_flow_12 = flow_12[0].detach().cpu().numpy().transpose([1, 2, 0])
+        np_flow_21 = flow_21[0].detach().cpu().numpy().transpose([1, 2, 0])
+        # vx = np_flow_12[:, :, 0]
+        # vy = np_flow_12[:, :, 1]
+        # f = open(os.path.join(r'G:\ARFlow-master\data\flow_dataset\ceshi_tmp', name + '_vx.bin'), 'wb')
+        # vx.astype(np.float32).tofile(f)
+        # f.close()
+        #
+        # f = open(os.path.join(r'G:\ARFlow-master\data\flow_dataset\ceshi_tmp', name + '_vy.bin'), 'wb')
+        # vy.astype(np.float32).tofile(f)
+        # f.close()
+        occu_mask_12 = occu_mask1[0].detach().cpu().numpy().transpose([1, 2, 0])
+        occu_mask_21 = occu_mask2[0].detach().cpu().numpy().transpose([1, 2, 0])
+        back_occu_mask_1 = back_occu_mask1[0].detach().cpu().numpy().transpose([1, 2, 0])
+        back_occu_mask_2 = back_occu_mask2[0].detach().cpu().numpy().transpose([1, 2, 0])
+
+        vis_flow_12 = flow_to_image(np_flow_12)
+        vis_flow_21 = flow_to_image(np_flow_21)
+
+        row1 = np.concatenate([np.uint8(imgs[0]), np.uint8(imgs[1]), np.uint8(vis_flow_12), np.uint8(vis_flow_21)], axis=1)
+        # row2 = np.concatenate([np.uint8(np_warped_image12), np.uint8(np_warped_image21), 255*np.tile(occu_mask_12, (1, 1, 3)),
+        #                        255*np.tile(occu_mask_21, (1, 1, 3))], axis=1)
+        row2 = np.concatenate([np.uint8(np_warped_image12), np.uint8(np_warped_image21), 255 * np.tile(back_occu_mask_1, (1, 1, 3)),
+             255 * np.tile(back_occu_mask_2, (1, 1, 3))], axis=1)
+        image_matrix = np.concatenate([row1, row2], axis=0)
+
+        imageio.imsave(name, image_matrix)
+        # imageio.imsave('warped_image.jpg', warped_image)
+
+        # fig = plt.figure()
+        # plt.imshow(image_matrix)
+        # plt.show()
